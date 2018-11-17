d72489bb9c1e6b6d46afb44ec2d5694a02deba38
Mike Kistler <mkistler@us.ibm.com>
2018-11-16 18:56:24 -0600

    Add Playgrounds for remaining services

---
+++ AssistantV2.playground/Contents.swift
@@ -0,0 +1,20 @@
+//:# Watson Assistant V2
+
+import PlaygroundSupport
+
+//: This playground contains code examples for the Watson Assistant V1 service.
+
+// Enable support for asynchronous completion handlers
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+//: To use the Watson Developer Cloud Swift SDK in your application, specify it in your `Cartfile`:
+//:```
+//:github "watson-developer-cloud/swift-sdk"
+//:```
+
+//:Then run the following command to build the dependencies and frameworks:
+//:```bash
+//:carthage update --platform iOS
+//:```
+
+import AssistantV2
